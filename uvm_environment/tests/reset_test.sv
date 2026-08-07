6ac1e57d73fdbd20f7e81a7f5dcbbc3239326a6b
Ketan Jamdar <jamdarketan@gmail.com>
2026-08-08 03:14:57 +0530

    Add files via upload

---
+++ uvm_environment/tests/reset_test.sv
@@ -0,0 +1,41 @@
+`ifndef RESET_TEST_SV
+`define RESET_TEST_SV
+
+class reset_test extends base_test;
+
+    //--------------------------------------------
+    // Factory Registration
+    //--------------------------------------------
+    `uvm_component_utils(reset_test)
+
+    //--------------------------------------------
+    // Constructor
+    //--------------------------------------------
+    function new(string name="reset_test",
+                 uvm_component parent=null);
+
+        super.new(name,parent);
+
+    endfunction
+
+    //--------------------------------------------
+    // Run Phase
+    //--------------------------------------------
+    task run_phase(uvm_phase phase);
+
+        phase.raise_objection(this);
+
+        `uvm_info(get_type_name(),
+                  "Running Power-On Reset Test",
+                  UVM_LOW)
+
+        // Wait for reset to complete
+        #100ns;
+
+        phase.drop_objection(this);
+
+    endtask
+
+endclass
+
+`endif
