7fdf3d2d78d36b06d0ea75712c88ca4f2a30d503
Ketan Jamdar <jamdarketan@gmail.com>
2026-08-13 23:37:23 +0530

    Add files via upload

---
+++ uvm_environment/ahb/ahb_sequencer.sv
@@ -0,0 +1,26 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+
+import ahb_apb_pkg::*;
+
+
+`ifndef AHB_SEQUENCER_SV
+`define AHB_SEQUENCER_SV
+
+class ahb_sequencer extends uvm_sequencer #(ahb_transaction);
+
+    // Factory Registration
+    `uvm_component_utils(ahb_sequencer)
+
+    //---------------------------------------------
+    // Constructor
+    //---------------------------------------------
+    function new(string name = "ahb_sequencer",
+                 uvm_component parent = null);
+        super.new(name, parent);
+    endfunction
+
+endclass
+
+`endif
